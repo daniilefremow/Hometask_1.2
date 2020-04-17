ee2abd9508013756ad0bd24fb83f26f6e9414f41
DaniilEfremow <klevermilka@gmail.com>
2020-04-17 21:16:29 +0300

    Add Hometask_1.2

---
+++ Hometask_1.2/Hometask_1.2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Hometask2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку:");
+            string str = Console.ReadLine();
+            string[] num = str.Split(' ');
+
+            Console.Write("Четные элементы:");
+
+            for (int i = 0; i < num.Length; i++)
+            {
+                if (int.Parse(num[i]) % 2 == 0)
+                {
+                    Console.Write(" {0}", num[i]);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
